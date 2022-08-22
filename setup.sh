35c06486e43eeb78ca843c2f2ee05eff2034b957
Defy <61808223+De-y@users.noreply.github.com>
2022-08-22 13:48:58 -0700

    V1, The Governance Update
    The Governance Update features the base Konnect system &
    
    * GET function to get files from the internet built into shell
    * Account creation & management system
    * Shell(work in progress) 
    * SETUP Wizard
    
    Expect more features to come! We are currently working on a auto-updater & a connection through tor.

---
+++ setup.sh
@@ -0,0 +1,8 @@
+mkdir users
+sudo apt install python3
+sudo apt install python-pip3
+pip install pywget
+pip install pytz
+pip install tqdm
+sudo chmod -x launch.sh
+echo "Finished setup. Please run launch.sh"
